c4a63bf94f7ad36ad21a74ed79dbc9b0d1d9cffd
Thatcher Clough <thatcher.clough@gmail.com>
2024-09-12 14:59:59 -0500

    Added a line of code that does literally nothing

--- app/src/main/java/com/cs407/helloworld/MainActivity.kt
+++ app/src/main/java/com/cs407/helloworld/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var img: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
